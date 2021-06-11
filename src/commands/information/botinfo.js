89c9a565d1aab0098cc7b2bc15b127d8979f8c8e
Dennis5618 <TropicoDiscord@protonmail.com>
2021-06-11 19:01:14 +0200

    Updated botinfo.ks

--- src/commands/information/botinfo.js
+++ src/commands/information/botinfo.js
@@ -1,23 +1,30 @@
-const { MessageEmbed, version } = require("discord.js")
+const { MessageEmbed, version } = require("discord.js");
+const { MessageButton } = require("discord-buttons");
 
 module.exports = {
     name: "botinfo",
     category: "information",
     description: "Shows some statistics of the bot",
     run: async (client, message) => {
-        message.channel.send(new MessageEmbed()
+        const Github = new MessageButton()
+            .setStyle("url")
+            .setLabel("Github repository")
+            .setURL("https://github.com/Dennis-5618/Popcorn")
+
+        const embed = new MessageEmbed()
             .setColor("#5865F2")
             .setDescription(`
-        > **Bot information**
-        Prefix: \`p!\`
-        Server count: \`${client.guilds.cache.size.toLocaleString()}\`
-        User count: \`${client.users.cache.size.toLocaleString()}\`
-        Command count: \`${client.commands.size}\`
+    > **Bot information**
+    Prefix: \`p!\`
+    Server count: \`${client.guilds.cache.size.toLocaleString()}\`
+    User count: \`${client.users.cache.size.toLocaleString()}\`
+    Command count: \`${client.commands.size}\`
 
-        > **System information**
-        Memory usage: \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\`
-        Discord.js version: \`v${version}\`
-        `)
-        );
+    > **System information**
+    Memory usage: \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB\`
+    Discord.js version: \`v${version}\`
+    `)
+
+        message.channel.send({ embed, button: Github });
     }
 };
