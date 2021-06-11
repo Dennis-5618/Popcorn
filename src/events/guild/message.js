220ea111dd9dda841b98de8aef7f72a5ed8b6937
Dennis5618 <TropicoDiscord@protonmail.com>
2021-06-11 19:52:52 +0200

    Added response when the bot is pinged

--- src/events/guild/message.js
+++ src/events/guild/message.js
@@ -4,6 +4,7 @@ const economy = require("../../schemas/economy");
 module.exports = {
     name: "message",
     run: async (client, message) => {
+        if (message.mentions.members.first() == client.user.id) return message.channel.send("My prefix is `p!`")
         if (message.author.bot || !message.guild || !message.content.toLowerCase().startsWith("p!")) return;
 
         const data = await economy.findOne({ User: message.author.id });
@@ -31,6 +32,5 @@ module.exports = {
         } catch (error) {
             return message.channel.send(`An error has occured: \`${error.message}\``);
         };
-
     }
 };
