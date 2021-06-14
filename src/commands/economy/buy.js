459b9fe277c99140f526b8bbb8be114eda227770
Dennis5618 <TropicoDiscord@protonmail.com>
2021-06-14 13:19:32 +0200

    Fixed the buy command

--- src/commands/economy/buy.js
+++ src/commands/economy/buy.js
@@ -6,15 +6,18 @@ module.exports = {
     category: "economy",
     description: "buys the specified item and adds it to your inventory",
     run: async (client, message, args) => {
-        const item = shopItems.find((value) => value.id);
+        const item = shopItems.find((value) => value.id == args[0].toLowerCase());
         if (!item) return message.channel.send("I couldn't find that item, please make sure to provide the item ID");
 
         const data = await economy.findOne({ User: message.author.id });
         if (data.Wallet < item.price) return message.channel.send(`You currently don't have enough money in your wallet to buy the ${item.item}`);
-        if (data.userInventory.includes(item.id)) return message.channel.send("You already have that item in your inventory");
 
+        const hasItem = Object.keys(data.userInventory).includes(item.id);
+        if (hasItem) return message.channel.send("You already have that item in your inventory");
+        
         await economy.findOneAndUpdate({ User: message.author.id }, {
-            userInventory: { [item.id]: 1 }
+            userInventory: { [item.id]: 1 },
+            $inc: { Wallet: -[item.price] }
         });
     }
 };
