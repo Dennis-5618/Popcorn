fb633395d7e7f94f5efbe1b031ea23284cd0ffdc
Dennis5618 <TropicoDiscord@protonmail.com>
2021-06-30 12:36:11 +0200

    Added messageDelete and messageUpdate events

---
+++ src/events/guild/messageDelete.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const settings = require("../../schemas/settings");
+
+module.exports = {
+    name: "messageDelete",
+    run: async (client, message) => {
+        const data = await settings.findOne({ Guild: message.guild.id });
+        if (!data || message.partial) return;
+
+        const logChannel = client.channels.cache.get(data.LogChannel);
+
+        const embed = new MessageEmbed()
+            .setColor("#FF4C4C")
+            .setAuthor(message.author.tag, message.author.avatarURL({ dynamic: true }))
+            .addField("Channel:", message.channel)
+            .addField("Content:", message.content)
+            .setFooter(`Message ID: ${message.id}`)
+            .setTimestamp()
+
+        logChannel.send(embed);
+    }
+};
---
+++ src/events/guild/messageUpdate.js
@@ -0,0 +1,22 @@
+const { MessageEmbed } = require("discord.js");
+const settings = require("../../schemas/settings");
+
+module.exports = {
+    name: "messageUpdate",
+    run: async (client, oldMessage, newMessage) => {
+        const data = await settings.findOne({ Guild: newMessage.guild.id });
+        if (!data) return;
+
+        const logChannel = client.channels.cache.get(data.LogChannel);
+
+        const embed = new MessageEmbed()
+        .setColor("#5865F2")
+        .setAuthor(newMessage.author.tag, newMessage.author.avatarURL({ dynamic: true }))
+        .addField("Before:", oldMessage.content)
+        .addField("After:", newMessage.content)
+        .setFooter(`Message ID: ${newMessage.id}`)
+        .setTimestamp()
+
+        logChannel.send(embed);
+    }
+};
