7551967f9062bbaf5805b79360c05b582ae7b86e
Dennis5618 <TropicoDiscord@protonmail.com>
2021-07-07 17:02:05 +0200

    Added option to clear messages of mentioned user

--- src/commands/moderation/clear.js
+++ src/commands/moderation/clear.js
@@ -1,3 +1,4 @@
+const { MessageEmbed } = require("discord.js");
 const ms = require("ms");
 
 module.exports = {
@@ -8,14 +9,33 @@ module.exports = {
     userPermissions: ["MANAGE_MESSAGES"],
     botPermissions: ["MANAGE_MESSAGES"],
     run: async (client, message, args) => {
-        if (!args.length || isNaN[args[0]] || parseInt(args[0]) > 100 || parseInt(args[0]) < 0) return message.channel.send("Please specify the amount of messages to be deleted as a number between 1 and 100");
+        const user = message.mentions.members.first();
 
-        const messages = await message.channel.messages.fetch({ limit: parseInt(args[0]) });
-        const usable = messages.filter((m) => (m.createdTimestamp - Date.now()) < ms("14d") && !m.pinned);
+        if (user) {
+            const messages = await message.channel.messages.fetch({ limit: args[1] });
+            const usable = messages.filter((m) => (m.author.id == user.id) && !m.pinned);
 
-        await message.delete();
-        await message.channel.bulkDelete(usable);
+            await message.delete()
+            await message.channel.bulkDelete(usable);
 
-        message.channel.send(`Successfully deleted \`${args[0]}\` messages`).then(msg => { msg.delete({ timeout: 3000 }) });
+            return message.channel.send(new MessageEmbed()
+                .setColor("#5865F2")
+                .setDescription(`Successfully deleted ${usable.size} messages of ${user}`)
+            );
+
+        } else {
+            if (!args.length || isNaN[args[0]] || parseInt(args[0]) > 100 || parseInt(args[0]) < 0) return message.channel.send("Please specify the amount of messages you want to delete as a number above 0");
+
+            const messages = await message.channel.messages.fetch({ limit: args[0] });
+            const usable = messages.filter((m) => (m.createdTimestamp - Date.now()) < ms("14d") && !m.pinned)
+
+            await message.delete();
+            await message.channel.bulkDelete(usable);
+
+            return message.channel.send(new MessageEmbed()
+                .setColor("#5865F2")
+                .setDescription(`Successfully deleted ${usable.size} messages`)
+            );
+        };
     }
 };
