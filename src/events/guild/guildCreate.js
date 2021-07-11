c4fa15331a784d5e3e9557371e2f64878660657d
Dennis5618 <TropicoDiscord@protonmail.com>
2021-07-12 00:38:49 +0200

    Added a console.log for joining/leaving servers

--- src/events/guild/guildCreate.js
+++ src/events/guild/guildCreate.js
@@ -2,5 +2,7 @@ module.exports = {
     name: "guildCreate",
     run: (client) => {
         client.user.setActivity(`${client.guilds.cache.size} servers`, { type: "WATCHING" });
+
+        console.log(`Joined a new server! | Total servers: ${client.guilds.cache.size}`);
     }
 };
--- src/events/guild/guildDelete.js
+++ src/events/guild/guildDelete.js
@@ -2,5 +2,7 @@ module.exports = {
     name: "guildDelete",
     run: (client) => {
         client.user.setActivity(`${client.guilds.cache.size} servers`, { type: "WATCHING" });
+
+        console.log(`Left a server :( | Total servers: ${client.guilds.cache.size}`);
     }
 };
