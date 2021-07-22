c2ee6e523b8bf074217d6acd023ab97fc6134c47
Dennis5618 <TropicoDiscord@protonmail.com>
2021-07-22 15:45:51 +0200

    Added a reply for the restart command if the user isnt the bot owner

--- src/commands/utility/restart.js
+++ src/commands/utility/restart.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "restart",
     category: "utility",
     run: async (client, message) => {
-        if (message.author.id != "294821968887152641") return;
+        if (message.author.id != "294821968887152641") return message.channel.send("This command is only available for the bot owner");
         await message.channel.send("Restarting...");
         process.exit();
     }
